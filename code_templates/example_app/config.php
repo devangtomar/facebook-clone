729c0d099bee5e37fa3b6b13d25750464e98f574
d0t077o <devangtomar123@gmail.com>
2018-02-01 19:59:01 +0530

    adjoining  the file config.php

---
+++ code_templates/example_app/config.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Common configuration, functions, and definitions file
+ */
+session_start();
+require_once('phpforfb_framework.php');
+
+/**
+ * Configuration section
+ *
+ * Configure your Facebook application data here
+ */
+//APP_NAME identifies the application's canvas (http://apps.facebook.com/APP_NAME)
+define('APP_NAME', 'examplephpforme');
+define('APP_TITLE', 'ExamplePHPforMe');
+define('APP_ID', '');
+define('APP_SECKEY', '');
+
+/**
+ * Definitions section
+ *
+ * Place constants here that are commonly used throughout your project
+ */
+
+/**
+ * Functions section
+ *
+ * Place functions here that are commonly used throughout your project
+ */
+?>
