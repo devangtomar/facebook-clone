f0a2cca7708d325b8385aceda34b85612b8de6f7
d0t077o <devangtomar123@gmail.com>
2018-02-15 19:59:03 +0530

    adding  the file index.php

---
+++ code_templates/example_full/mobile/index.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * PHPforFB
+ * - a professional PHP Framework for Facebook�
+ * 
+ *
+ * = Code template for a facebook application + page app =
+ *
+ * This PHP code template is a complete Facebook application.
+ */
+
+/**
+ * Entry point for the mobile web version of the facebook application
+ * - initializations for the mobile modus and then jumps into ../index.php
+ */
+include('../config.php');
+
+if($FacebookAPP->lastErrorCode>0){
+	//Creation failed => Display error message and exit
+	echo "PHPforFB Error: ".$FacebookAPP->lastErrorCode." -> ".$FacebookAPP->lastError;
+	exit;
+}else{
+
+	if(($res=$FacebookAPP->ForcePermissions('basic'))===FALSE){
+	    echo "PHPforFB Error: ".$FacebookAPP->lastErrorCode." -> ".$FacebookAPP->lastError;
+	}else{
+		if($res == 1){
+			$_SESSION['isMobile'] = TRUE;
+			Header('Location: ../');
+		}else{
+			echo 'Please try again and click on accept.';
+		}
+	}
+}
+?>
