d027a5ec67a41889a8d3484dbf3967f2e8173947
d0t077o <devangtomar123@gmail.com>
2018-02-19 19:59:03 +0530

    updating  the file config.php

---
+++ code_templates/example_page/config.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Common configuration, functions, and definitions file
+ */
+session_start();
+require_once('phpforfb_framework.php');
+
+/**
+ * Configuration section
+ *
+ * Configure your Facebook application data here
+ */
+//APP_NAME identifies the application's canvas (http://apps.facebook.com/APP_NAME)
+define('APP_NAME', 'examplephpforme');
+define('APP_TITLE', 'ExamplePHPforMe');
+define('APP_ID', '');
+define('APP_SECKEY', '');
+//Set APP_APIKEY to the value from
+//https://www.facebook.com/developers/apps.php?app_id=APP_ID
+define('APP_APIKEY', '');
+
+/**
+ * Definitions section
+ *
+ * Place constants here that are commonly used throughout your project
+ */
+
+/**
+ * Functions section
+ *
+ * Place functions here that are commonly used throughout your project
+ */
+?>
