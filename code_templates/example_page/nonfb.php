d37bd16844b6a432e2b16ac23abaaaa5ce3aa453
d0t077o <devangtomar123@gmail.com>
2018-02-25 19:59:04 +0530

    adding  the file nonfb.php

---
+++ code_templates/example_page/nonfb.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This is an include file.
+ * This file provides the non-Facebook variant of the application,
+ * i.e. when it was not entered through Facebook.
+ */
+
+echo 'Here in nonfb.php you can insert your code for a non-Facebook version.<br>
+<br>
+You can display different informations here depending on which part of the application included this file.<br>
+
+Included by '.$show.'.php
+';
+
+?>
